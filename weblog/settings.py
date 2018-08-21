259d29761d2ae4cd4d1f29f834fa67319f4694ff
red9bit <Khashayar.rajaee@gmail.com>
2018-08-21 20:18:55 +0430

    restore back SECRET_KEY into settings file

--- weblog/settings.py
+++ weblog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = KEY
+SECRET_KEY = 'emys=pvyifnq!9$d_ic3_yemmd3!+zs(r3-vxyx8%46y4d35)u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
