151540e7bc6cddcf5b3dddf1509cc2687682cbba
red9bit <Khashayar.rajaee@gmail.com>
2018-08-22 12:37:43 +0430

    creating the base views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Post, Comment, Like
+from django.views.decorators.http import require_GET
+from django.views.decorators.csrf import csrf_exempt
+from django.http.response import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'blog/index.html')
+
+
+@require_GET
+def posts(request):
+    return render(request, 'blog/posts.html')
+
+
+@require_GET
+def post(request, year, month, day, title):
+    return HttpResponse('OK, that regex was correct year: {}, month: {}, day: {}, title: {}'
+                        .format(year, month, day, title))
