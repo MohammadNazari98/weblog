85414c6935c09face22c8da4be21cb804e892e62
red9bit <Khashayar.rajaee@gmail.com>
2018-08-28 13:20:26 +0430

    some sort of cleaning in models

--- blog/models.py
+++ blog/models.py
@@ -1,31 +1,33 @@
 from django.db import models
-from django.conf.global_settings import AUTH_USER_MODEL
-
-DRAFT_STATUS = 1
-PUBLISH_STATUS = 2
-
-STATUS_CHOICES = (
-    (DRAFT_STATUS, 'Draft'),
-    (PUBLISH_STATUS, 'Publish'),
-)
+from weblog.settings import AUTH_USER_MODEL
 
 
 class PublishedPostsManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(is_enable=True, status=2).order_by('-last_modified')
+        return super().get_queryset().filter(
+            is_enable=True, status=Post.PUBLISH_STATUS).order_by('-last_modified')
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=120, null=False, blank=False)
-    content = models.TextField(null=False, blank=False)
+    DRAFT_STATUS = 1
+    PUBLISH_STATUS = 2
+
+    STATUS_CHOICES = (
+        (DRAFT_STATUS, 'Draft'),
+        (PUBLISH_STATUS, 'Publish'),
+    )
+
+    title = models.CharField(max_length=120)
+    content = models.TextField()
     created_time = models.DateTimeField('created time', auto_now_add=True)
     last_modified = models.DateTimeField('updated time', auto_now=True)
     # TODO: publish time
     status = models.PositiveSmallIntegerField(choices=STATUS_CHOICES, default=1)
     author = models.ForeignKey(AUTH_USER_MODEL)
+    is_enable = models.BooleanField(default=True)
+
     published = PublishedPostsManager()
     objects = models.Manager()
-    is_enable = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.author} --> {self.title}'
@@ -43,14 +45,15 @@ class ActivesCommentsManager(models.Manager):
 
 
 class Comment(models.Model):
-    content = models.TextField(null=False, blank=False)
+    content = models.TextField()
     created_time = models.DateTimeField('sent time', auto_now_add=True)
-    name = models.CharField(max_length=50, null=False, blank=False)
-    email = models.EmailField(null=False, blank=False)
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
     post = models.ForeignKey(Post, related_name='comments')
+    is_enable = models.BooleanField(default=True)
+
     actives = ActivesCommentsManager()
     objects = models.Manager()
-    is_enable = models.BooleanField(default=True)
 
     class Meta:
         unique_together = ('post', 'email')
@@ -60,8 +63,8 @@ class Comment(models.Model):
 
 
 class Like(models.Model):
-    name = models.CharField(max_length=50, null=False, blank=False)
-    email = models.EmailField(null=False, blank=False)
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
     is_like = models.BooleanField(default=True)
     post = models.ForeignKey(Post, related_name='likes')
 
